09dbec976ec64b6630e8a2aada649f53ecc38c33
Phuong Nguyen <phuonguyen@nvidia.com>
2025-08-20 07:41:32 -0700

    revert https://github.com/AI-Hypercomputer/maxtext/commit/a33a0fe82e71bb8b967801616697be7244498a75
    Signed-off-by: Phuong Nguyen <phuonguyen@nvidia.com>

--- MaxText/layers/linears.py
+++ MaxText/layers/linears.py
@@ -444,8 +444,19 @@ class MlpBlock(nnx.Module):
     """Applies Transformer MlpBlock module."""
     cfg = self.config
 
+
     if self.mlp_layer_norm is not None:
       inputs = self.mlp_layer_norm(inputs)
+      if self.model_mode == MODEL_MODE_PREFILL:
+        inputs = nn.with_logical_constraint(inputs, ("activation_batch",
+                                                     "prefill_activation_norm_length",
+                                                     "activation_embed")
+                                            )
+      else:
+        inputs = nn.with_logical_constraint(inputs, ("activation_batch",
+                                                     "activation_norm_length",
+                                                     "activation_embed")
+                                            )
 
     # Iterate over specified MLP input activation functions.
     # e.g. ('relu',) or ('gelu', 'linear') for gated-gelu.
